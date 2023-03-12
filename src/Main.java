0b14a8d8a119196623c5b26df5dd016c8f1c13ff
Asghar Nazir <asgharkrk@gmail.com>
2023-03-12 17:45:25 -0400

    Sleep added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+            Thread.sleep(1000);
+        }
+
     }
 }
